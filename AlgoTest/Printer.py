0f5fe637a5899604896fc4e48632e9a75b6c00a5
yesiamjulie <yesiamjulie0@gmail.com>
2020-01-02 00:01:54 +0900

    Add commit

--- AlgoTest/Printer.py
+++ AlgoTest/Printer.py
@@ -16,3 +16,25 @@ def solution(priorities, location):
             else:
                 location -= 1
     return answer
+
+''' other code
+def solution(p, l):
+    ans = 0
+    m = max(p)
+    while True:
+        v = p.pop(0)
+        if m == v:
+            ans += 1
+            if l == 0:
+                break
+            else:
+                l -= 1
+            m = max(p)
+        else:
+            p.append(v)
+            if l == 0:
+                l = len(p)-1
+            else:
+                l -= 1
+    return ans
+'''
