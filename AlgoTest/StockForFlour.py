3f6c97d375266e686d1a2e87d0146adc0b1f5f00
yesiamjulie0 <yesiamjulie0@gmail.com>
2020-02-06 13:43:54 +0900

    Add Algorithm Test

---
+++ AlgoTest/StockForFlour.py
@@ -0,0 +1,15 @@
+import heapq
+def solution(stock, dates, supplies, k):
+    answer = 0
+    idx=0
+    h=[]
+    while(stock<k):
+        for i in range(idx,len(dates)):
+            if dates[i]<=stock:
+                heapq.heappush(h,(-supplies[i],supplies[i]))
+                idx=i+1
+            else:
+                break
+        stock+=heapq.heappop(h)[1]
+        answer+=1
+    return answer
---
+++ AlgoTest/heapQueue1.py
@@ -0,0 +1,21 @@
+import heapq
+
+def solution(scoville, K):
+
+    heapq.heapify(scoville)
+    answer = 0
+
+    while True:
+        first = heapq.heappop(scoville)
+
+        if first >= K:
+            break
+        if len(scoville) == 0:
+            return -1
+
+        second = heapq.heappop(scoville)
+        heapq.heappush(scoville, first + second * 2)
+        answer += 1
+
+
+    return answer
